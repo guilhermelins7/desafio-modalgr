27c6f68170abf840757e9fa1367477bbf9b21063
guilhermelins7 <guilherme7lins@gmail.com>
2025-02-01 22:50:34 -0300

    refactor: componente e-mail imcorporado à cadastro
    A lógica de componente do campo e-mail foi adicionado ao cadastro para implementar o alert dos campos obrigatorios.

--- src/app/view/cadastro/cadastro.component.ts
+++ src/app/view/cadastro/cadastro.component.ts
@@ -2,8 +2,25 @@ import {Component} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
-import { EmailComponent } from '../email/email.component';
 import { CepComponent } from '../cep/cep.component';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-cadastro',
@@ -13,11 +30,17 @@ import { CepComponent } from '../cep/cep.component';
     MatInputModule,
     NgxMaskDirective,
     NgxMaskPipe,
-    EmailComponent,
-    CepComponent
+    CepComponent,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+    nameFormControl = new FormControl('', [Validators.required]);
+    emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  
+    matcher = new MyErrorStateMatcher();
 }
